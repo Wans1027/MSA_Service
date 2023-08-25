993699160abb8ee291a28ce4f5788525c6df6a34
Wan1027 <rladhkstmd10@daum.net>
2023-08-25 23:38:56 +0900

    Create Index by memberId

--- ChattingService/src/main/java/ChattingService/entity/ChatParticipation.java
+++ ChattingService/src/main/java/ChattingService/entity/ChatParticipation.java
@@ -2,6 +2,7 @@ package ChattingService.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,18 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @NoArgsConstructor
+@Table(indexes = @Index(name = "index_memberId", columnList = "memberId"))
 public class ChatParticipation {
     @Id
     @GeneratedValue
     @Column(name = "chatPart_id")
     private Long id;
-
+    @NotNull
+    private Long memberId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chattingRoom_id")
     private ChattingRoom chattingRoom;
-
-    private Long memberId;
 
     public ChatParticipation(ChattingRoom chattingRoom, Long memberId) {
         this.chattingRoom = chattingRoom;
